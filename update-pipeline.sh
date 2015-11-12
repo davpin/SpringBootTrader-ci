88948dac659caf1a2bba85a55e1070d792cffc78
dpinto-pivotal <dpinto@pivotal.io>
2015-11-12 16:46:04 +0000

    externalizing properties

--- update-pipeline.sh
+++ update-pipeline.sh
@@ -1,1 +1,1 @@
-fly set-pipeline -p SpringBootTrader-development -c development-pipeline.yml -v "springboottrader-ci-repo-private-key=$(cat id_rsa)"
+fly set-pipeline -p SpringBootTrader-development -c development-pipeline.yml -v "springboottrader-ci-repo-private-key=$(cat id_rsa)" -l properties.yml
