39f9a1e029d2b51d419df5a6e3589d6688f14bed
dpinto-pivotal <dpinto@pivotal.io>
2015-11-14 09:24:47 +0000

    testing deploy job

--- update-pipeline.sh
+++ update-pipeline.sh
@@ -1,1 +1,1 @@
-fly set-pipeline -p SpringBootTrader-development -c development-pipeline.yml -v "springboottrader-ci-repo-private-key=$(cat id_rsa)" -l properties.yml
+fly set-pipeline -p SpringBootTrader-development -c development-pipeline.yml -v "springboottrader-ci-repo-private-key=$(cat id_rsa)" -v "cf-password=ert123" -l properties.yml
